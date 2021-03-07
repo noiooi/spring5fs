0c3a0fb3f59203191950d059a73cb806e39fbb2a
noiooi <noiooi426@gmail.com>
2021-03-07 17:59:05 +0900

    feat: ch09 스프링 MVC 시작하기
    - 이클립스 톰캣 설정 (v8.5.63)
    - @EnableWebMvc 애노테이션을 통한 스프링 MVC 기본적인 구성 설정
    - '/hello' GET 메서드에 대한 매핑 설정 및 JSP를 통한 결과 확인
    - 오류 처리
    -- The server cannot be started because one or more of the ports are invalid. Open the server editor and correct the invalid ports.
    -- 톰캣 admin port 지정(8085)

---
+++ sp5-chap09/src/main/java/chap09/HelloController.java
@@ -0,0 +1,17 @@
+package chap09;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller // 스프링 MVC 컨트롤러 지정
+public class HelloController {
+
+	@GetMapping("/hello") // 메서드가 처리할 요청 경로 지정
+	public String hello(Model model,
+			@RequestParam(value = "name", required = false) String name) {
+		model.addAttribute("greeting", "안녕하세요, " + name);
+		return "hello";
+	}
+}
